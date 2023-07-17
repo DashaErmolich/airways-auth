a68e19816b6c5e7281305d380dd0cfca62d9be79
DashaErmolich <dasha.ermolich@gmail.com>
2023-07-17 15:57:27 +0200

    fix: bind the router db to the app

--- index.js
+++ index.js
@@ -7,11 +7,13 @@ const server = jsonServer.create()
 const router = jsonServer.router(path.join(__dirname, 'db.json'))
 const middlewares = jsonServer.defaults()
 
+server.db = router.db
+
 server.use(cors())
 server.use(jsonServer.bodyParser)
 server.use(middlewares)
-server.use(router)
 server.use(auth)
+server.use(router)
 
 const PORT = 8000
 
